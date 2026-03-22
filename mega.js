3380cb34d48ed160f7a369803d42bcefd586a2e9
yumirashanith2007-dot <yumirashanith2007@gmail.com>
2026-03-22 16:07:48 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'yumirashanith2007@gmail.com',
+    password: 'pramoddananjaya2005@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
